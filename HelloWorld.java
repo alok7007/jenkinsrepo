a850686c954e975f7fcd80557f0b02116fead254
Alok(aj907g) <alok.jha@amdocs.com>
2017-11-08 22:54:01 -0500

    Added HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author ALOKJH
+ *
+ */
+public class HelloWorld
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Alok How Are You.");
+        
+    }
+    
+}
